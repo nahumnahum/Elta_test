42034dd60ee4233132410c66c75de87e8817a9df
Nahum <641724ab@gmail.com>
2024-02-15 12:58:06 +0200

    PlatformService

---
+++ micro services/PlatformService/Data/PrepDb.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using PlatformService.Models;
+
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if(!context.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding Data...");
+
+                context.Platforms.AddRange(
+                    new Platform() { Name = "dot net", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
+                );
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> We already have data");
+            }
+        }
+    }
+}
---
+++ micro services/PlatformService/Profiles/PlatformProfile.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using PlatformService.Models;
+using PlatformService.Dtos;
+
+namespace PlatformService.Profiles
+{
+	public class PlatformProfile : Profile
+	{
+        public PlatformProfile()
+        {
+            // Source -> Target
+            CreateMap<Platform, PlatformReadDto>();
+			CreateMap<PlatformReadDto, Platform>();
+		}
+    }
+}
