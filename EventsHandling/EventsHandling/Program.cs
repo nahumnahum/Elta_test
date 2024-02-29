ded3f2554698d47e579ac08ca068a631cffc6e15
Nahum <641724ab@gmail.com>
2024-02-29 19:18:43 +0200

    Event handling program

---
+++ EventsHandling/EventsHandling/Program.cs
@@ -0,0 +1,53 @@
+﻿public class ButtonClickedEventArgs : EventArgs
+{
+    public string ButtonName { get; set; }
+
+    public ButtonClickedEventArgs(string buttonName)
+    {
+        ButtonName = buttonName;
+    }
+}
+
+
+public class Button
+{
+
+    public event EventHandler<ButtonClickedEventArgs> ButtonClicked;
+
+    public void Click()
+    {
+        OnButtonClicked(new ButtonClickedEventArgs("MyButton"));
+    }
+
+    protected virtual void OnButtonClicked(ButtonClickedEventArgs e)
+    {
+        ButtonClicked?.Invoke(this, e);
+    }
+}
+
+
+public class ButtonListener
+{
+    public ButtonListener(Button button)
+    {
+        button.ButtonClicked += Button_ButtonClicked;
+    }
+
+    private void Button_ButtonClicked(object sender, ButtonClickedEventArgs e)
+    {
+        Console.WriteLine($"Button '{e.ButtonName}' was clicked!");
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Button myButton = new Button();
+
+        ButtonListener buttonListener = new ButtonListener(myButton);
+
+        myButton.Click();
+    }
+}
