dadd772482d7a5d5f2e75c0727d32ec90f06e346
Nahum <641724ab@gmail.com>
2024-03-05 14:18:14 +0200

    a separate module for 'ParsedDataEventArgs'

---
+++ TextDataParser/TextDataParser/ParsedDataEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TextDataParser
+{
+    public class ParsedDataEventArgs : EventArgs
+    {
+        public IParsedData ParsedData { get; }
+
+        public ParsedDataEventArgs(IParsedData parsedData)
+        {
+            ParsedData = parsedData;
+        }
+    }
+}
