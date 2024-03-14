d1b6845825bc604c947230785fdd4a460554cf9b
Nahum <641724ab@gmail.com>
2024-03-14 17:37:49 +0200

    Web socket client-server

---
+++ WebSocketClient/WebSocketClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using WebSocketSharp;
+
+namespace SocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebSocket webSocket = new WebSocket("ws://127.0.0.1:7890/Echo"))
+            {
+                webSocket.OnMessage += webSocket_OnMessage;
+
+                webSocket.Connect();
+
+                webSocket.Send("Hello Server!");
+
+                Console.ReadKey();
+            }
+        }
+
+        private static void webSocket_OnMessage(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine("Received from the server: " + e.Data);
+        }
+    }
+}
---
+++ WebSocketServer/WebSocketServer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace SocketServer
+{
+    class Program
+    {
+        public class Echo : WebSocketBehavior
+        {
+            protected override void OnMessage(MessageEventArgs e)
+            {
+                Console.WriteLine("Received message from Echo client: " + e.Data);
+                Send(e.Data);
+            }
+        }
+
+        public class EchoAll : WebSocketBehavior
+        {
+            protected override void OnMessage(MessageEventArgs e)
+            {
+                Console.WriteLine("Received message from EchoAll client: " + e.Data);
+                Sessions.Broadcast(e.Data);
+            }
+        }
+        static void Main(string[] args)
+        {
+            WebSocketServer socketServer = new WebSocketServer("ws://127.0.0.1:7890");
+
+            socketServer.AddWebSocketService<Echo>("/Echo");
+            socketServer.AddWebSocketService<EchoAll>("/EchoAll");
+
+            socketServer.Start();
+
+            Console.WriteLine("Server started on ws://127.0.0.1g:7890/Echo");
+            Console.WriteLine("Server started on ws://127.0.0.1g:7890/EchoAll");
+
+            Console.ReadKey();
+
+            socketServer.Stop();
+        }
+    }
+}
